4d44543f2536878fa6310cd06c4b9602e9358721
kavithachinnusamy20 <kavithachinnusamy20@gmail.com>
2025-07-09 23:02:37 -0400

    Fetch book data from API and render the book details in Card

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -4,10 +4,34 @@ import Modal from './Modal';
 const Card = ({ book }) => {
   const [show, setShow] = useState(false);
   const [bookItem, setItem] = useState(null);
-
+  
   return (
     <>
-     
+    {book.map((item, index) => {
+      const thumbnail =  item.volumeInfo.imageLinks?.smallThumbnail;
+      const amount = item.saleInfo.listPrice?.amount;
+      
+      if (thumbnail && amount) {
+        return (
+          <div
+          key={index}
+          className="card"
+          onClick={() => {
+            setShow(true);
+            setItem(item);
+          }}
+          >
+               <img src={thumbnail} alt="" />
+                <div className="bottom">
+                    <h3 className="title">{item.volumeInfo.title}</h3>
+                    <p className="amount">&#8377;{amount}</p>
+                </div>
+          </div>
+        );
+      }
+      return null;
+    })}
+    <Modal show={show} item={bookItem} onClose={() => setShow(false)} />
     </>
   );
 };
