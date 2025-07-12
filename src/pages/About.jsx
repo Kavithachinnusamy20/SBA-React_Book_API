681e257d7b407da65c1e95201d18811758bdb9fc
kavithachinnusamy20 <kavithachinnusamy20@gmail.com>
2025-07-11 23:46:55 -0400

    Final commit

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,8 @@
 import react from "react";
 function About(){
     return( 
-        <div>
-            <h2>About This Website</h2>
+        <div className="category-text">
+            <h2 >About This Website</h2><br></br>
             <h4>Google Books uses various categories and genres to organize its vast collection of books. These categories help users discover and browse books based on their interests, including literary genres, subject areas, and more</h4>
         </div>
     );
